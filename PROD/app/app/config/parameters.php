0fbcc35a4db17c4771da86eb430624594d34630f
a23danielgs <a23danielgs@iessanclemente.net>
2025-03-04 01:19:48 +0100

    GRANcommit5

---
+++ PROD/app/app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'sql7.freesqldatabase.com',
+    'database_port' => '',
+    'database_name' => 'sql7765786',
+    'database_user' => 'sql7765786',
+    'database_password' => 'e7852pGbma',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'mUZUxSFR2nbfp3QOmfcYGVSVop9u5mF494ZwEMVaStgd6AmT0VNpUK2749ryvKm8',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2025-03-04',
+    'locale' => 'es-ES',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'S5xcIuOzRnFqW0xLZNsDBB6ouoHn3Ek0AWtuPvSCBbbZd8q8ko1kMoaLLUKFpSCL',
+    'cookie_iv' => 'Ne10iqAmhEE2fL7x49CL8RBvR40vF5vt',
+    'new_cookie_key' => 'def00000c6d60fdf12d43b57393bb12210d76b68d1dea6d056ba4db97145bdaae0706aa793bef7bf7f261c0dc393177fce36a85d2c5d9194f01997af6d2d3b879c5672b5',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr/KZ6nmGnRqJJWF9IDCx
+L8yAEwiIv8XvGlYzAHFFuw851CeFIVVsUDJ/AjoT+0TmaEK5V4UaFf1TnDdHWjR2
+GGu1VmN97RgMb8VvfrlNC7mTz9YJq3YuHMRAhfpciDGe2Fmn3LQaVpcY/O/Zpdi0
+Szio8j11gfA8X1ugAaKxzQ3s2IF8E66rxRTjK33Nf+/jze07zCfhY3gSpmeMeGyl
+GopHfdtFY7kFumt+TmQ9SynsdJp0fuinoWoRtD0ZC99AsVvGCPhzw4+ZRL/VAutQ
+Ewnxgl7WbCOSgh5vK64UwfxzQTbotIFbsiBvhLiQDFeSVhvWtIgaDOx2rgPR6Jl0
++QIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCv8pnqeYadGokl
+YX0gMLEvzIATCIi/xe8aVjMAcUW7DznUJ4UhVWxQMn8COhP7ROZoQrlXhRoV/VOc
+N0daNHYYa7VWY33tGAxvxW9+uU0LuZPP1gmrdi4cxECF+lyIMZ7YWafctBpWlxj8
+79ml2LRLOKjyPXWB8DxfW6ABorHNDezYgXwTrqvFFOMrfc1/7+PN7TvMJ+FjeBKm
+Z4x4bKUaikd920VjuQW6a35OZD1LKex0mnR+6KehahG0PRkL30CxW8YI+HPDj5lE
+v9UC61ATCfGCXtZsI5KCHm8rrhTB/HNBNui0gVuyIG+EuJAMV5JWG9a0iBoM7Hau
+A9HomXT5AgMBAAECggEAA1XzohTkvvTCVeGXyDT0HwZfBT1brlsX0eWBeQgsvMOo
+ooOPTBb9AzY53zZvq4teXOqq7eRZnsIbRDJuGWE2PfdIzFb9i7Gxhqr1Fq93rPm7
+Oi0CKOyIKkZ0PLYG7BQYwVxNyOrnNu4NbLo1Y6YMoQJ1fYY5xJlj46Dukylc+pi5
+cLP/MrLxjGYjGKCR6+nquh12B0IZO8QqCmXIxSpiuAa9eC72juNYDOltfff1JMcg
+PmD4cOXCGpJb04ddpQlLPbfOZX7oc7uChXKvzvWZLSSvw9WPCmHxCjUmvem5Xvw4
+Y9eJTDaDJLIj5e0yRzef/zxBiK6jgEOjeXu6HHEW2wKBgQDYxBLAjYkVvV0ogZIR
+h+E2793CI8egu1S6qKjaGxd0Q/PpMWsgYunLUAP571SOOzsJNHxb6v6+Ld09xffD
+NTdVLdRBkVA9RSlg1wPF/Mz5+vrL1xnMSYl/zAzswH35FnzcxKpDFNFbspXDx82Q
+og2OtgJlrmNV6Y9weoTgFGjCSwKBgQDPyzMa+z/OAF18fUedybuCufciJnGqEnlM
+9JzE6w24Uz9sBAe8x400zMqJiynTyoviA2VRkvIM0lWhk/zGWrzg5oUf3Al5A7Zr
+VApMs57FnOC0cqVjJ6dGf6KsILZ8+pj0vDrSYqMMoGXFYC6jcBP9D52pSN0hfhfz
+8RxWPJv7SwKBgF/VH609ZbPUylDUJ+3UMv+EwHfKNcGb0HPNiQkUHSRrvnQEQyZI
+VLBsKbXf13QiP5FsQOaqWAFgaYWb9lO5PUNNRZ/PTLgp0Ydcx/BKOZzzFtnzuRYW
+FNIJNlWdLJRpGr7Hu7Z4h66Qn6G0ve8JlhDX6utlU8lxvtatc/1VSJNNAoGAJT00
+VnSKlb+JAYNuLz/1A9OSD6O2IRWYeYspAT1iwxLgPdpnDJTyoHDHJ2kyQMt+0C1t
+nfXovRWA1SkxVtbyHJgTaYdEh1R7/7Ya9m4GWUHpH02giCmWF9NN8u0V50X93XZS
+UFvu/pdMRdgb45x1aJfA2JK/Vn5Diqs+OdI43gsCgYEAzV1rGoZiRyQzSNPmP+8V
+HLClWtNA9b9YEMUO7d2gI4d9Xpx5Mx/zBpzDy9LZBi+P/rQFwfTEp7kiYdjToHS/
+mvi7JNvv07T0WlTqlde3TNTGiND/k3c1xQc7pBz9Zxj87gm0vrbqogDitbKeMTAM
+a+y74EfRNKwGJrdLEwIhkec=
+-----END PRIVATE KEY-----
+',
+  ),
+);
